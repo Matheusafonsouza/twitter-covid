0924a598e27b169c24d01d79c997561fd5d305d6
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-14 02:17:22 -0300

    chore: sending tweets

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
-import settings
-from services import CoronaService
+from services import CoronaService, TweetService
+from utils import create_corona_message
 
+# create corona service instance and get info from data website
 corona_service = CoronaService()
 corona_brazil_info = corona_service.get_info('brazil')
 corona_world_info = corona_service.get_info()
 
-print(corona_brazil_info)
-print(corona_world_info)
+# format messages for tweet service message
+corona_brazil_message = create_corona_message(corona_brazil_info)
+corona_world_message = create_corona_message(corona_world_info)
+
+# create tweet service instance and tweet messages
+tweet_service = TweetService()
+tweet_service.tweet(corona_brazil_info)
+tweet_service.tweet(corona_world_message)
