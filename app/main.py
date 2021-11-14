6a5c794307328e487736b076b9e5a4fdc34fa8fd
matheusafonsouza <matheusafonsouza@gmail.com>
2021-11-14 16:27:45 -0300

    refactor: tasks file

--- app/main.py
+++ app/main.py
@@ -1,16 +1,3 @@
-from services import CoronaService, TweetService
-from utils import create_corona_message
+from tasks import corona_task
 
-# create corona service instance and get info from data website
-corona_service = CoronaService()
-corona_brazil_info = corona_service.get_info('brazil')
-corona_world_info = corona_service.get_info()
-
-# format messages for tweet service message
-corona_brazil_message = create_corona_message(corona_brazil_info)
-corona_world_message = create_corona_message(corona_world_info)
-
-# create tweet service instance and tweet messages
-tweet_service = TweetService()
-tweet_service.tweet(corona_brazil_info)
-tweet_service.tweet(corona_world_message)
+corona_task()
---
+++ app/tasks.py
@@ -0,0 +1,22 @@
+from services import CoronaService, TweetService
+from utils import create_corona_message
+
+
+def corona_task():
+    """
+    Task which will get corona infos from website and post it on the
+    twitter profile
+    """
+    # create corona service instance and get info from data website
+    corona_service = CoronaService()
+    corona_brazil_info = corona_service.get_info('brazil')
+    corona_world_info = corona_service.get_info()
+
+    # format messages for tweet service message
+    corona_brazil_message = create_corona_message(corona_brazil_info)
+    corona_world_message = create_corona_message(corona_world_info)
+
+    # create tweet service instance and tweet messages
+    tweet_service = TweetService()
+    tweet_service.tweet(corona_brazil_info)
+    tweet_service.tweet(corona_world_message)
